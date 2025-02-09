7cd875f60373e036dd2e244b18f4d74a4f3a4b6a
Lizaveta Bokan <liz07bokan@gmail.com>
2025-02-09 20:59:45 +0100

    fix: remove @Public()

--- src/db/db.controller.ts
+++ src/db/db.controller.ts
@@ -4,16 +4,14 @@ import { HasRoles } from 'src/auth/decorators/has-roles.decorator';
 import { Role } from 'src/auth/enums/role.enum';
 import { CrudDto } from './dto/crud.dto';
 import { ValidObjectId } from 'src/helpers/decorators/valid-object-id.decorator';
-import { Public } from 'src/auth/decorators/publicity.decorator';
 import { transformObject } from 'src/helpers/helpers';
 
 @Controller('db')
 export class DBController {
   constructor(private dbService: DBService) {}
 
-  @Public()
   @Post('/crud')
-  // @HasRoles(Role.Admin)
+  @HasRoles(Role.Admin)
   async crud(
     @Body() crudDto: CrudDto,
     @ValidObjectId() id?: string
