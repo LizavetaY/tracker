2809e18962baacf6e1f7cc5c04dc68afda8e07be
Lizaveta Bokan <liz07bokan@gmail.com>
2025-02-10 18:51:31 +0100

    fix: solve comments

--- src/db/db.service.ts
+++ src/db/db.service.ts
@@ -11,39 +11,24 @@ export class DBService {
   async crud({ id, crudDto }: ICrudParams): Promise<any> {
     const collection: Collection = this.connection.collection(crudDto.collection);
 
-    if (!collection) {
-      throw new BadRequestException(`Collection ${crudDto.collection} not found.`);
-    }
-
-    const crudOperation = crudDto.operation || '';
-    let result = null;
-
-    switch (crudOperation) {
+    switch (crudDto.operation) {
       case CRUD.Get:
-        result = await collection.find(crudDto.filter || {}).toArray();
-
-        return result;
+        return collection.find(crudDto.filter || {}).toArray();
       case CRUD.Post:
-        result = await collection.insertOne(crudDto.body || {});
-
-        return result?.insertedId;
+        return collection.insertOne(crudDto.body || {});
       case CRUD.Put:
         const updateObjectId = new Types.ObjectId(id);
 
-        result = await collection.updateOne(
+        return collection.updateOne(
           { _id: updateObjectId },
           { $set: crudDto.body },
         );
-
-        return updateObjectId;
       case CRUD.Delete:
         const deleteObjectId = new Types.ObjectId(id);
 
-        result = await collection.deleteOne({ _id: deleteObjectId });
-
-        return deleteObjectId;
+        return collection.deleteOne({ _id: deleteObjectId });
       default:
-        return result;
+        return null;
     }
   }
 }
